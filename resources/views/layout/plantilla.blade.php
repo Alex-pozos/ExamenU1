ae7de7bf165390f4d73fe442344dbde604059a18
Alex-Pozos <alejandro.po.ra@gmail.com>
2021-09-26 16:06:31 -0500

    ExamenU1

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('img/laravel.png') }}" type="image/x-icon">
     {{-- Estilos proprios en CSS --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     {{-- bootstrap style --}}
